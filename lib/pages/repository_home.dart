bd736ce69d411f1a6fba8c8090caecd14b62fc6b
Thomas <souahthomas@gmail.com>
2024-05-10 08:50:22 +0000

    some clean code

--- lib/pages/repository_home.dart
+++ lib/pages/repository_home.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class Repository_home extends StatefulWidget {
   String login;
   Repository_home({super.key, required this.login});
@@ -23,33 +22,30 @@ class _Repository_homeState extends State<Repository_home> {
   }
 
   @override
-  Widget build (BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Repository de  ${widget.login}'),
-        backgroundColor: Colors.blue,
-        actions: [
-
-        ],
-      ),
-      body: ListView.separated(
-          itemBuilder: (context, index) => ListTile(
-            title: Text("${dataRepository[index]['name']}"),
-          ),
-          separatorBuilder: (context, index) => const Divider(height: 2, color: Colors.green,),
-          itemCount: dataRepository == null ? 0 : dataRepository.length
-      )
-    );
+        appBar: AppBar(
+          title: Text('Repository de  ${widget.login}'),
+          backgroundColor: Colors.blue,
+          actions: [],
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text("${dataRepository[index]['name']}"),
+                ),
+            separatorBuilder: (context, index) => const Divider(
+                  height: 2,
+                  color: Colors.green,
+                ),
+            itemCount: dataRepository == null ? 0 : dataRepository.length));
   }
 
   Future<void> loadRepository() async {
-
     Uri url = Uri.https('api.github.com', '/users/${widget.login}/repos', {});
     var response = await http.get(url);
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       setState(() {
         dataRepository = jsonDecode(response.body) as List<dynamic>;
-        print(dataRepository[0]['name']);
       });
     } else {
       print('Response failed with code : ${response.statusCode}');
